0cdd70950b5d123923ad9626dfcd1dc22aab90d4
Elia <98826251+Elia1802@users.noreply.github.com>
2025-01-20 14:09:16 +0100

    Update Connect Command
    You can use:
    /server [SERVER] or
    /server [SERVER] [PLAYER]

--- src/main/java/de/elia/commands/connect/ConnectCommand.java
+++ src/main/java/de/elia/commands/connect/ConnectCommand.java
@@ -29,13 +29,78 @@ public class ConnectCommand extends Command {
   @Override
   public boolean execute(@NotNull CommandSender sender, @NotNull String s, @NotNull String[] args) {
     if (sender instanceof Player player) {
-      if (args.length == 2) {
-        var target = Bukkit.getPlayer(args[0]);
+      if (args.length == 1) {
+        if (args[0].equalsIgnoreCase("event")) {
+          if (player.hasPermission("soultransfer.connect.event")) {
+            //convert the bukkit player to a craft player
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            //Send to the client packets to change the server
+            craftPlayer.getHandle().connection.sendPacket(new ClientboundTransferPacket("162.55.253.235", 25563));
+            //Message send
+            craftPlayer.sendMessage(gray("You are moved to the ").append(aqua("Event Server")).append(gray("!")));
+            Main.instance.logger.logInfo(craftPlayer.getName() + "moved to the Event Server (IP: 162.55.253.235 | Port: 25563)");
+            return true;
+          }else {
+            player.sendMessage(red("You don't have the permission to connect to this server!"));
+            return false;
+          }
+        }else if (args[0].equalsIgnoreCase("soul")) {
+          //convert the bukkit player to a craft player
+          CraftPlayer craftPlayer = (CraftPlayer) player;
+          //Send to the client packets to change the server
+          craftPlayer.getHandle().connection.sendPacket(new ClientboundTransferPacket("162.55.253.235", 25564));
+          //Message send
+          craftPlayer.sendMessage(gray("You are moved to the ").append(aqua("Soul Server")).append(gray("!")));
+          Main.instance.logger.logInfo(craftPlayer.getName() + "moved to the Soul Server (IP: 162.55.253.235 | Port: 25564)");
+          return true;
+        }else if (args[0].equalsIgnoreCase("build")) {
+          if (player.hasPermission("soultransfer.connect.build")) {
+            //convert the bukkit player to a craft player
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            //Send to the client packets to change the server
+            craftPlayer.getHandle().connection.sendPacket(new ClientboundTransferPacket("162.55.253.235", 25562));
+            //Message send
+            craftPlayer.sendMessage(gray("You are moved to the ").append(aqua("Build Server")).append(gray("!")));
+            Main.instance.logger.logInfo(craftPlayer.getName() + "moved to the Build Server (IP: 162.55.253.235 | Port: 25562)");
+            return true;
+          }else {
+            player.sendMessage(red("You don't have the permission to connect to this server!"));
+            return false;
+          }
+        }else if (args[0].equalsIgnoreCase("test")) {
+          if (player.hasPermission("soultransfer.connect.test")){
+            //convert the bukkit player to a craft player
+            CraftPlayer craftPlayer = (CraftPlayer) player;
+            //Send to the client packets to change the server
+            craftPlayer.getHandle().connection.sendPacket(new ClientboundTransferPacket("162.55.253.235", 25561));
+            //Message send
+            craftPlayer.sendMessage(gray("You are moved to the ").append(aqua("Test Server")).append(gray("!")));
+            Main.instance.logger.logInfo(craftPlayer.getName() + "moved to the Test Server (IP: 162.55.253.235 | Port: 25561)");
+            return true;
+          }else {
+            player.sendMessage(red("You don't have the permission to connect to this server!"));
+            return false;
+          }
+        }else if (args[0].equalsIgnoreCase("lobby")) {
+          //convert the bukkit player to a craft player
+          CraftPlayer craftPlayer = (CraftPlayer) player;
+          //Send to the client packets to change the server
+          craftPlayer.getHandle().connection.sendPacket(new ClientboundTransferPacket("162.55.253.235", 25565));
+          //Message send
+          craftPlayer.sendMessage(gray("You are moved to the ").append(aqua("Lobby")).append(gray("!")));
+          Main.instance.logger.logInfo(craftPlayer.getName() + "moved to the Lobby (IP: 162.55.253.235 | Port: 25565)");
+          return true;
+        }else {
+          player.sendMessage(red("This server does exist!"));
+          return false;
+        }
+      }else if (args.length == 2) {
+        var target = Bukkit.getPlayer(args[1]);
         if (target == null){
           player.sendMessage(red("This Player does not exist!"));
           return false;
         }
-        if (args[1].equalsIgnoreCase("event")) {
+        if (args[0].equalsIgnoreCase("event")) {
           if (target.hasPermission("soultransfer.connect.event")){
             //convert the bukkit target player to a craft player
             CraftPlayer craftTarget = (CraftPlayer) target;
@@ -49,7 +114,7 @@ public class ConnectCommand extends Command {
             player.sendMessage(red("You don't have the permission to connect to this server!"));
             return false;
           }
-        }else if (args[1].equalsIgnoreCase("soul")) {
+        }else if (args[0].equalsIgnoreCase("soul")) {
           //convert the bukkit target player to a craft player
           CraftPlayer craftTarget = (CraftPlayer) target;
           //Send to the client packets to change the server
@@ -58,7 +123,7 @@ public class ConnectCommand extends Command {
           craftTarget.sendMessage(gray("You are moved to the ").append(aqua("Soul Server")).append(gray("!")));
           Main.instance.logger.logInfo(craftTarget.getName() + "moved to the Soul Server (IP: 162.55.253.235 | Port: 25564)");
           return true;
-        }else if (args[1].equalsIgnoreCase("build")) {
+        }else if (args[0].equalsIgnoreCase("build")) {
           if (target.hasPermission("soultransfer.connect.build")){
             //convert the bukkit target player to a craft player
             CraftPlayer craftTarget = (CraftPlayer) target;
@@ -72,7 +137,7 @@ public class ConnectCommand extends Command {
             player.sendMessage(red("You don't have the permission to connect to this server!"));
             return false;
           }
-        }else if (args[1].equalsIgnoreCase("test")) {
+        }else if (args[0].equalsIgnoreCase("test")) {
           if (target.hasPermission("soultransfer.connect.test")){
             //convert the bukkit target player to a craft player
             CraftPlayer craftTarget = (CraftPlayer) target;
@@ -86,7 +151,7 @@ public class ConnectCommand extends Command {
             player.sendMessage(red("You don't have the permission to connect to this server!"));
             return false;
           }
-        }else if (args[1].equalsIgnoreCase("lobby")) {
+        }else if (args[0].equalsIgnoreCase("lobby")) {
           //convert the bukkit target player to a craft player
           CraftPlayer craftTarget = (CraftPlayer) target;
           //Send to the client packets to change the server
@@ -96,11 +161,12 @@ public class ConnectCommand extends Command {
           Main.instance.logger.logInfo(craftTarget.getName() + "moved to the Lobby (IP: 162.55.253.235 | Port: 25565)");
           return true;
         }else {
-          player.sendMessage(red("This Server does not exist!"));
+          player.sendMessage(red("This server does exist!"));
           return false;
         }
       }else {
-        player.sendMessage(red("Please use /connect [PLAYER] [SERVER)"));
+        player.sendMessage(red("Please use /connect [SERVER]"));
+        player.sendMessage(red("Please use /connect [SERVER] [PLAYER]"));
         return false;
       }
     }else {
@@ -115,15 +181,15 @@ public class ConnectCommand extends Command {
     ArrayList<String> tab1 = new ArrayList<>();
     ArrayList<String> tab2 = new ArrayList<>();
     if (args.length == 1) {
-      Bukkit.getOnlinePlayers().forEach(player -> tab1.add(player.getName()));
+      tab1.add("event");
+      tab1.add("soul");
+      tab1.add("build");
+      tab1.add("test");
+      tab1.add("lobby");
       return tab1;
     }
     if (args.length == 2) {
-      tab2.add("event");
-      tab2.add("soul");
-      tab2.add("build");
-      tab2.add("test");
-      tab2.add("lobby");
+      Bukkit.getOnlinePlayers().forEach(player -> tab2.add(player.getName()));
       return tab2;
     }
     return null;
